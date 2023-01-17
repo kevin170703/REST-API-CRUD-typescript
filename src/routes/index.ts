6e468f114170f7cea36a178d265e980d7f4285e8
kevin170703 <correakevinfabian01@gmail.com>
2023-01-17 09:33:22 -0300

    add router delete

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,10 +5,12 @@ import {
   getUsers,
   getUserById,
   crateUser,
+  delateUser,
 } from "../controllers/index.controllers";
 
 router.get("/users", getUsers);
 router.get("/users/:id", getUserById);
 router.post("/users", crateUser);
+router.delete("/users/:id", delateUser);
 
 export default router;
