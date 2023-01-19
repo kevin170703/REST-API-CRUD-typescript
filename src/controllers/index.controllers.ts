de080250b9f18047924aeab5dc6e1cd5c196cddd
kevin170703 <correakevinfabian01@gmail.com>
2023-01-19 12:00:15 -0300

    actualizacion de usuarios

--- src/controllers/index.controllers.ts
+++ src/controllers/index.controllers.ts
@@ -32,17 +32,28 @@ export const crateUser = async (
   res: Response
 ): Promise<Response> => {
   const { name, email } = req.body;
-  const respose: QueryResult = await pool.query(
-    "INSERT INTO users (name, email) VAlUES($1, $2)",
-    [name, email]
-  );
+  await pool.query("INSERT INTO users (name, email) VAlUES($1, $2)", [
+    name,
+    email,
+  ]);
   return res.status(200).json({ msj: "Usuario creado correactamente" });
 };
 
-// export const updateUser = async (
-//   req: Request,
-//   res: Response
-// ): Promise<Response> => {};
+export const updateUser = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const { id } = req.params;
+  const { name, email } = req.body;
+  await pool.query("UPDATE users SET name = $1, email= $2 WHERE id = $3 ", [
+    name,
+    email,
+    id,
+  ]);
+  return res.json({
+    msj: `el usuario con la id ${id} se actualizo correctamente`,
+  });
+};
 
 export const delateUser = async (
   req: Request,
