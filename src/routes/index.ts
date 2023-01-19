7720e061ef5dd7634a623d5351e0488597ebe791
kevin170703 <correakevinfabian01@gmail.com>
2023-01-19 12:00:47 -0300

    ruta para actualizar usuarios

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,11 +6,13 @@ import {
   getUserById,
   crateUser,
   delateUser,
+  updateUser,
 } from "../controllers/index.controllers";
 
 router.get("/users", getUsers);
 router.get("/users/:id", getUserById);
 router.post("/users", crateUser);
+router.put("/users/:id", updateUser);
 router.delete("/users/:id", delateUser);
 
 export default router;
