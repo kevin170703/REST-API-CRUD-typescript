a4dc11f534157afc8ed237bbb1c5276904862de3
kevin170703 <correakevinfabian01@gmail.com>
2023-01-20 10:20:44 -0300

    index mofifi

---
+++ build/routes/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const router = (0, express_1.Router)();
+const index_controllers_1 = require("../controllers/index.controllers");
+router.get("/users", index_controllers_1.getUsers);
+router.get("/users/:id", index_controllers_1.getUserById);
+router.post("/users", index_controllers_1.crateUser);
+router.put("/users/:id", index_controllers_1.updateUser);
+router.delete("/users/:id", index_controllers_1.delateUser);
+exports.default = router;
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import indexRoutes from "./routes/index";
 
 const app = express();
-const PORT: number = 3000;
+const PORT = 3000;
 
 // middlewares
 app.use(express.json());
@@ -13,13 +13,3 @@ app.use(indexRoutes);
 app.listen(PORT, () => {
   console.log(`server en el puesto ${PORT}`);
 });
-
-// CREATE TABLE users(
-//   id SERIAL PRIMARY KEY,
-//   name VARCHAR(40),
-//   email TEXT
-// );
-
-// INSERT INTO users (name, email)
-//   VALUES ('joe', 'joe@ibm.com'),
-//          ('ryan', 'ryan@faztweb.com');
